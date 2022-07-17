6f4db04254fa844661d457108a4566c3617f2d39
damiru2 <chalkarov11@mail.ru>
2022-07-17 21:44:40 +0500

    learned some features of segmentedControl by code

--- UISegmentedControl/ViewController.swift
+++ UISegmentedControl/ViewController.swift
@@ -9,9 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let titles = ["one", "two", "three"]
+        segmentedControl = UISegmentedControl(items: titles)
+        
+        segmentedControl.insertSegment(withTitle: "four", at: 3, animated: true)
+        
+        segmentedControl.setTitle("two2", forSegmentAt: 1)
+        
+        segmentedControl.setImage(UIImage(systemName: "circle"), forSegmentAt: 0)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.selectedSegmentTintColor = UIColor.red
+//         segmentedControl.setWidth(150, forSegmentAt: 2)
+        
+        segmentedControl.setTitle("so long title text", forSegmentAt: 2)
+        segmentedControl.apportionsSegmentWidthsByContent = true
+        segmentedControl.addTarget(self, action: #selector(segmentAction), for: .valueChanged)
+        
+        
+        
+        view.addSubview(segmentedControl)
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+                                     segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+                                     segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16)])
+        
+        
+    }
+    
+    @objc func segmentAction(sender: UISegmentedControl) {
+        print(sender.titleForSegment(at: sender.selectedSegmentIndex))
     }
 
 
